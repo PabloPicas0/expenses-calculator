7124fab8246ec2e84a3700535935db59272ca749
PabloPicas0 <pawel.fe@gmail.com>
2023-12-18 22:33:40 +0100

    feat: add render income and spendings individualy

--- src/Components/Spendings.tsx
+++ src/Components/Spendings.tsx
@@ -1,14 +1,21 @@
-import { ReactNode } from "react";
-
 type Props = {
-  children?: ReactNode;
+  data: number[];
 };
 
-const Spendings = ({ children }: Props) => {
-  return <div>
-    {children}
-  </div>;
+const Spendings = (props: Props) => {
+  const { data } = props;
+
+  // This can be simplyfied to one reduce method
+  const income = data.filter((e, idx) => idx % 2 === 0);
+  const spending = data.filter((e, idx) => idx % 2 !== 0);
+
+  return (
+    <div className="spendings-wrapper">
+      <div id="income">{income}</div>
+
+      <div id="spendings">{spending}</div>
+    </div>
+  );
 };
 
-
-export default Spendings
+export default Spendings;
