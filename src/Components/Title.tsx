f0d5af4b62b7dd8f2797440505e7ea30c2cb0295
PabloPicas0 <pawel.fe@gmail.com>
2023-12-19 23:15:42 +0100

    feat: add style for income/expense wrappers

--- src/Components/Title.tsx
+++ src/Components/Title.tsx
@@ -1,13 +1,26 @@
 const Title = () => {
-  const currentYear = new Date().toLocaleDateString("en-GB", { month: "long", year: "numeric" });
+  const currentDate = new Date().toLocaleDateString("en-GB", { month: "long", year: "numeric" });
 
   return (
     <header className="header">
-      <h1>Budget available in {currentYear}</h1>
-      <p>Income</p>
-      <p>Expenses</p>
+      <h1>
+        Budget available in {currentDate}
+        <br />+ $1000
+      </h1>
+
+      <div style={{ maxWidth: "420px", margin: "10px auto 0px auto" }}>
+        <div className="header-income-wrapper spending-details">
+          <p>Income</p>
+          <p>+1000</p>
+        </div>
+
+        <div className="header-expenses-wrapper spending-details">
+          <p>Expenses</p>
+          <p>-1000</p>
+        </div>
+      </div>
     </header>
   );
 };
 
-export default Title
+export default Title;
