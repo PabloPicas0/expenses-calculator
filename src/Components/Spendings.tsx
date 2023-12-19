715585e49f9625a15770f130e3f7ba120595cd93
PabloPicas0 <pawel.fe@gmail.com>
2023-12-19 23:17:17 +0100

    feat: change how data is sorted, add new styles

--- src/Components/Spendings.tsx
+++ src/Components/Spendings.tsx
@@ -1,35 +1,47 @@
-type Props = {
+type PropsTypes = {
   data: number[];
 };
 
-const Spendings = (props: Props) => {
+type ReduceAccumulatorTypes = {
+  income: number[];
+  spending: number[];
+};
+
+const Spendings = (props: PropsTypes) => {
   const { data } = props;
 
-  // This can be simplyfied to one reduce method
-  const income = data.filter((e, idx) => idx % 2 === 0);
-  const spending = data.filter((e, idx) => idx % 2 !== 0);
+  const sortedData = data.reduce(
+    (acc, e, idx) => {
+      idx % 2 === 0 ? acc.income.push(idx) : acc.spending.push(idx);
+      return acc;
+    },
+    {
+      income: [],
+      spending: [],
+    } as ReduceAccumulatorTypes
+  );
 
   return (
     <div className="spendings-wrapper">
       <div id="income">
-        <h2 className="center-text">Income</h2>
-        {income.map((e) => {
+        <h2 className="center-text border-bottom padding-bottom color-green">Income</h2>
+        {sortedData.income.map((e) => {
           return (
-            <div key={e * Math.random()} id="earned" className="spending-details">
+            <div key={e * Math.random()} id="earned" className="spending-details border-bottom">
               <p>{Math.random() < 0.5 ? "Payment" : "lottery"}</p>
-              <p>{e}</p>
+              <p className="color-green">{e}</p>
             </div>
           );
         })}
       </div>
 
       <div id="spendings">
-        <h2 className="center-text">Spendings</h2>
-        {spending.map((e) => {
+        <h2 className="center-text border-bottom padding-bottom color-red">Spendings</h2>
+        {sortedData.spending.map((e) => {
           return (
-            <div key={e * Math.random()} id="spended" className="spending-details">
+            <div key={e * Math.random()} id="spended" className="spending-details border-bottom">
               <p>{Math.random() < 0.5 ? "Bills" : "Food"}</p>
-              <p>{e}</p>
+              <p className="color-red">{e}</p>
             </div>
           );
         })}
