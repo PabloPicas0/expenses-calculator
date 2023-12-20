124911b844455f1ef6410301591461b656a6bdc8
PabloPicas0 <pawel.fe@gmail.com>
2023-12-20 19:08:38 +0100

    feat: add styles for submit and history button

--- src/Components/Options.tsx
+++ src/Components/Options.tsx
@@ -1,16 +1,49 @@
 const Options = () => {
   return (
     <form className="options">
+      <button type="button" className="history-btn">
+        History
+        <svg
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="history-btn-icon">
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              d="M5.70711 9.71069C5.31658 10.1012 5.31658 10.7344 5.70711 11.1249L10.5993 16.0123C11.3805 16.7927 12.6463 16.7924 13.4271 16.0117L18.3174 11.1213C18.708 10.7308 18.708 10.0976 18.3174 9.70708C17.9269 9.31655 17.2937 9.31655 16.9032 9.70708L12.7176 13.8927C12.3271 14.2833 11.6939 14.2832 11.3034 13.8927L7.12132 9.71069C6.7308 9.32016 6.09763 9.32016 5.70711 9.71069Z"
+              fill="#0F0F0F"></path>{" "}
+          </g>
+        </svg>
+      </button>
+
       <select id="money">
         <option value="income">➕</option>
         <option value="expense">➖</option>
       </select>
 
-      <input type="text" placeholder="description" />
-      <input type="number" placeholder="ammount" />
+      <input type="text" placeholder="Description" className="input-base" />
+      <input type="number" placeholder="Ammount" min={0} className="input-base" />
 
-      <button>add</button>
-      <button>save</button>
+      <button className="icon-button" aria-label="submit">
+        <svg width={30} height={30} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              d="M8 5.00005C7.01165 5.00082 6.49359 5.01338 6.09202 5.21799C5.71569 5.40973 5.40973 5.71569 5.21799 6.09202C5 6.51984 5 7.07989 5 8.2V17.8C5 18.9201 5 19.4802 5.21799 19.908C5.40973 20.2843 5.71569 20.5903 6.09202 20.782C6.51984 21 7.07989 21 8.2 21H15.8C16.9201 21 17.4802 21 17.908 20.782C18.2843 20.5903 18.5903 20.2843 18.782 19.908C19 19.4802 19 18.9201 19 17.8V8.2C19 7.07989 19 6.51984 18.782 6.09202C18.5903 5.71569 18.2843 5.40973 17.908 5.21799C17.5064 5.01338 16.9884 5.00082 16 5.00005M8 5.00005V7H16V5.00005M8 5.00005V4.70711C8 4.25435 8.17986 3.82014 8.5 3.5C8.82014 3.17986 9.25435 3 9.70711 3H14.2929C14.7456 3 15.1799 3.17986 15.5 3.5C15.8201 3.82014 16 4.25435 16 4.70711V5.00005M12 11V17M9 14H15"
+              stroke="#008000"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"></path>{" "}
+          </g>
+        </svg>
+      </button>
     </form>
   );
 };
