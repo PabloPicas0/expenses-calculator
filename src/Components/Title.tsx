d3e543feb9598eedb1ba1418e16c6b98ee07e49c
PabloPicas0 <pawel.fe@gmail.com>
2023-12-23 19:59:31 +0100

    feat: title component calculate and display total income and spendings based on inserted data

--- src/Components/Title.tsx
+++ src/Components/Title.tsx
@@ -1,22 +1,33 @@
-const Title = () => {
-  const currentDate = new Date().toLocaleDateString("en-GB", { month: "long", year: "numeric" });
+import { PropsTypes } from "./Spendings";
+
+const Title = (props: PropsTypes) => {
+  const { data, month, year } = props;
+
+  const currentDate = new Date(`${year}-${month}-01`).toLocaleDateString("en-GB", {
+    month: "long",
+    year: "numeric",
+  });
+
+  const selectedData = data[year].filter((e) => e.month === month)[0];
+  const totalIncome = selectedData.income.reduce((acc, data) => acc + data.income, 0);
+  const totalSpendings = selectedData.spendings.reduce((acc, data) => acc + data.income, 0);
 
   return (
     <header className="header">
       <h1>
         Budget available in {currentDate}
-        <br />+ $1000
+        <br />$ {totalIncome - totalSpendings}
       </h1>
 
       <div style={{ maxWidth: "420px", margin: "10px auto 0px auto" }}>
         <div className="header-income-wrapper spending-details">
           <p>Income</p>
-          <p>+ $1000</p>
+          <p>+ ${totalIncome}</p>
         </div>
 
         <div className="header-expenses-wrapper spending-details">
           <p>Expenses</p>
-          <p>- $1000</p>
+          <p>- ${totalSpendings}</p>
         </div>
       </div>
     </header>
