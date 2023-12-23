a554ba117163ed83533bd49062751d86155cff78
PabloPicas0 <pawel.fe@gmail.com>
2023-12-23 18:47:33 +0100

    feat: add typings, initial data load form local storage and app state

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,52 @@
+import { useState } from "react";
 import "./App.scss";
 
 import Options from "./Components/Options";
 import Spendings from "./Components/Spendings";
 import Title from "./Components/Title";
 
-function App() {
-  const data = [...new Array(10)].map((e, idx) => idx);
+export type expensesType = {
+  [year: number | string]:
+    | {
+        month: string;
+        income: {
+          descritpiton: string;
+          income: number;
+        }[];
+        spendings: {
+          descritpiton: string;
+          income: number;
+        }[];
+      }[]
+    | [];
+};
 
+const storage = localStorage.getItem("expensesData");
+
+const expensesData: expensesType =
+  storage !== null
+    ? JSON.parse(storage)
+    : {
+        [new Date().getFullYear()]: [
+          {
+            month: new Date().toLocaleDateString("en-GB", { month: "short" }),
+            income: [],
+            spendings: [],
+          },
+        ],
+      };
+
+function App() {
+  const [data, setData] = useState(expensesData);
+  const [year, setYear] = useState(`${new Date().getFullYear()}`);
+  const [month, setMonth] = useState(new Date().toLocaleDateString("en-US", { month: "short" }));
+
+  console.log(data);
   return (
     <>
       <Title />
-      <Options />
-      <Spendings data={data} />
+      <Options year={year} month={month} setData={setData} />
+      <Spendings year={year} month={month} data={data} />
     </>
   );
 }
