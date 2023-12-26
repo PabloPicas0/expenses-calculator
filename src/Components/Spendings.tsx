1f33b64b0b25ac03b87bbd691f259591cccade42
PabloPicas0 <pawel.fe@gmail.com>
2023-12-26 23:28:18 +0100

    feat: add Cancel button component and silde animation on hover

--- src/Components/Spendings.tsx
+++ src/Components/Spendings.tsx
@@ -1,4 +1,5 @@
 import { expensesType } from "../App";
+import Cancel from "./Cancel";
 
 type PropsTypes = {
   year: string;
@@ -15,28 +16,40 @@ const Spendings = (props: PropsTypes) => {
   return (
     <div className="spendings-wrapper">
       <div id="income">
-        <h2 className="center-text border-bottom padding-bottom color-green">Income</h2>
+        <h2 className="center-text border-bottom padding-bottom color-green spendings-subtitle">Income</h2>
         {currentExpenses?.income.map((e) => {
           const { descritpiton, income } = e;
 
           return (
             <div key={income * Math.random()} id="earned" className="spending-details border-bottom">
               <p>{descritpiton}</p>
-              <p className="color-green">+ ${income}</p>
+
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <p className="color-green price">+ ${income}</p>
+                <button className="icon-button">
+                  <Cancel color="#008800" />
+                </button>
+              </div>
             </div>
           );
         })}
       </div>
 
       <div id="spendings">
-        <h2 className="center-text border-bottom padding-bottom color-red">Spendings</h2>
+        <h2 className="center-text border-bottom padding-bottom color-red spendings-subtitle">Spendings</h2>
         {currentExpenses?.spendings.map((e) => {
           const { descritpiton, income } = e;
 
           return (
             <div key={income * Math.random()} id="spended" className="spending-details border-bottom">
               <p>{descritpiton}</p>
-              <p className="color-red">- ${income}</p>
+
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <p className="color-red price">- ${income}</p>
+                <button className="icon-button">
+                  <Cancel color="#880000" />
+                </button>
+              </div>
             </div>
           );
         })}
