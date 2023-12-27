4f3c99e2e3b5a6d504475cd0f8bbdedcf6811094
PabloPicas0 <pawel.fe@gmail.com>
2023-12-27 21:02:15 +0100

    feat: add functionality for deleting individual spendings/incomes

--- src/Components/Spendings.tsx
+++ src/Components/Spendings.tsx
@@ -1,24 +1,49 @@
 import { expensesType } from "../App";
 import Cancel from "./Cancel";
 
-type PropsTypes = {
+type SpendingsPropsTypes = {
   year: string;
   month: string;
   data: expensesType;
+  setData: React.Dispatch<React.SetStateAction<expensesType>>;
 };
 
-const Spendings = (props: PropsTypes) => {
-  const { data, month, year } = props;
+type DeleteBtnIdTypes = [string, "income" | "spendings", string];
+
+const Spendings = (props: SpendingsPropsTypes) => {
+  const { data, month, year, setData } = props;
 
   const currentExpenses = data[year].find((e) => e.month === month);
   console.log(currentExpenses);
+
+  // TODO: If user selects spendings form history prevent him from delete this
+  const handleDelte = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const [_, transactionType, index] = e.currentTarget.id.split("-") as DeleteBtnIdTypes;
+
+    setData((prevData) => {
+      const newData: expensesType = {
+        ...prevData,
+        [year]: JSON.parse(JSON.stringify(data[year])),
+      };
+
+      const currentTransaction = newData[year].find((e) => e.month === month);
+
+      if (!currentTransaction) return prevData;
+
+      currentTransaction[transactionType].splice(Number(index), 1);
+
+      localStorage.setItem("expensesData", JSON.stringify(newData));
+
+      return newData;
+    });
+  };
 
   return (
     <div className="spendings-wrapper">
       <div id="income">
         <h2 className="center-text border-bottom padding-bottom color-green spendings-subtitle">Income</h2>
-        {currentExpenses?.income.map((e) => {
-          const { descritpiton, income } = e;
+        {currentExpenses?.income.map((currentIncome, index) => {
+          const { descritpiton, income } = currentIncome;
 
           return (
             <div key={income * Math.random()} id="earned" className="spending-details border-bottom">
@@ -26,7 +51,7 @@ const Spendings = (props: PropsTypes) => {
 
               <div style={{ display: "flex", alignItems: "center" }}>
                 <p className="color-green price">+ ${income}</p>
-                <button className="icon-button delete-btn">
+                <button id={`delte-income-${index}`} className="icon-button delete-btn" onClick={handleDelte}>
                   <Cancel color="#008800" />
                 </button>
               </div>
@@ -37,8 +62,8 @@ const Spendings = (props: PropsTypes) => {
 
       <div id="spendings">
         <h2 className="center-text border-bottom padding-bottom color-red spendings-subtitle">Spendings</h2>
-        {currentExpenses?.spendings.map((e) => {
-          const { descritpiton, income } = e;
+        {currentExpenses?.spendings.map((currentSpending, index) => {
+          const { descritpiton, income } = currentSpending;
 
           return (
             <div key={income * Math.random()} id="spended" className="spending-details border-bottom">
@@ -46,7 +71,10 @@ const Spendings = (props: PropsTypes) => {
 
               <div style={{ display: "flex", alignItems: "center" }}>
                 <p className="color-red price">- ${income}</p>
-                <button className="icon-button delete-btn">
+                <button
+                  id={`delete-spendings-${index}`}
+                  className="icon-button delete-btn"
+                  onClick={handleDelte}>
                   <Cancel color="#880000" />
                 </button>
               </div>
