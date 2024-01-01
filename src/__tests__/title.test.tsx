bc9ef70d746c00e240069bbded48569f8f3f0a1c
PabloPicas0 <pawel.fe@gmail.com>
2024-01-01 23:48:06 +0100

    test: create tests to title component

---
+++ src/__tests__/title.test.tsx
@@ -0,0 +1,40 @@
+import { describe, expect, test, vi } from "vitest";
+import Title from "../Components/Header/Title";
+import { render, screen } from "@testing-library/react";
+
+describe("Title component", () => {
+  test("it shows correct currency format", () => {
+    const setData = vi.fn();
+
+    render(
+      <Title
+        data={{
+          "2023": [
+            {
+              month: "Dec",
+              income: [
+                {
+                  descritpiton: "job",
+                  income: 6000,
+                },
+              ],
+              spendings: [
+                {
+                  descritpiton: "bills",
+                  income: 1345,
+                },
+              ],
+            },
+          ],
+        }}
+        year="2023"
+        month="Dec"
+      />
+    );
+
+    screen.debug() 
+
+    expect(screen.getByText("+ $6,000.00")).toBeInTheDocument();
+    expect(screen.getByText("- $1,345.00")).toBeInTheDocument();
+  });
+});
