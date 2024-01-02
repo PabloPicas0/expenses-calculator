e39e108269494b4adc9acc97f425d82c22936a84
PabloPicas0 <pawel.fe@gmail.com>
2024-01-02 16:30:08 +0100

    refactor: delete one import from vitest

--- src/__tests__/title.test.tsx
+++ src/__tests__/title.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, test, vi } from "vitest";
+import { describe, expect, test } from "vitest";
 import Title from "../Components/Header/Title";
 import { render, screen } from "@testing-library/react";
 
